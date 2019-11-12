1329c2160b09da94599c9d78c451b3037803f32c
BiBii <leeroy.bitbol@outlook.fr>
2019-11-12 13:06:23 +0100

    1st test

---
+++ Monopoly/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Monopoly
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
