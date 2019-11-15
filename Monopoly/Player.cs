2bf8bcebce467b209a80370ad85d50b12a291a84
Ariane Huckel <ari@MacBook-Pro-de-Ariane.local>
2019-11-15 14:31:08 +0100

    buycase

--- Monopoly/Player.cs
+++ Monopoly/Player.cs
@@ -53,12 +53,83 @@ namespace Monopoly
             List<string> colors = new List<string>();
             colors.Add("blanc");
             colors.Add("noir");
-            switch(newCase.Color)
+            colors.Add("Marron");
+            colors.Add("Bleu clair");
+            colors.Add("Rose");
+            colors.Add("Orange");
+            colors.Add("Rouge");
+            colors.Add("Jaune");
+            colors.Add("Vert");
+            colors.Add("Bleu fonce");
+            switch (newCase.Color)
             {
                 case "blanc":
                     while (properties[0][count] != null)
                         count++;
                     properties[0][count] = newCase;
+                    this.Balance -= newCase.PurchasePrice;
+                    break;
+
+                case "noir":
+                    while (properties[1][count] != null)
+                        count++;
+                    properties[1][count] = newCase;
+                    this.Balance -= newCase.PurchasePrice;
+                    break;
+
+                case "Marron":
+                    while (properties[2][count] != null)
+                        count++;
+                    properties[2][count] = newCase;
+                    this.Balance -= newCase.PurchasePrice;
+                    break;
+
+                case "Bleu clair":
+                    while (properties[3][count] != null)
+                        count++;
+                    properties[3][count] = newCase;
+                    this.Balance -= newCase.PurchasePrice;
+                    break;
+
+                case "Rose":
+                    while (properties[4][count] != null)
+                        count++;
+                    properties[4][count] = newCase;
+                    this.Balance -= newCase.PurchasePrice;
+                    break;
+
+                case "Orange":
+                    while (properties[5][count] != null)
+                        count++;
+                    properties[5][count] = newCase;
+                    this.Balance -= newCase.PurchasePrice;
+                    break;
+
+                case "Rouge":
+                    while (properties[6][count] != null)
+                        count++;
+                    properties[6][count] = newCase;
+                    this.Balance -= newCase.PurchasePrice;
+                    break;
+
+                case "Jaune":
+                    while (properties[7][count] != null)
+                        count++;
+                    properties[7][count] = newCase;
+                    this.Balance -= newCase.PurchasePrice;
+                    break;
+
+                case "Vert":
+                    while (properties[8][count] != null)
+                        count++;
+                    properties[8][count] = newCase;
+                    this.Balance -= newCase.PurchasePrice;
+                    break;
+
+                case "Bleu fonce":
+                    while (properties[9][count] != null)
+                        count++;
+                    properties[9][count] = newCase;
                     this.Balance -= newCase.PurchasePrice;
                     break;
             }
